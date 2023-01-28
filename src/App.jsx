34e88211a3cc2d09a8f24995106b02046867b4c9
West <agus2kcoin@outlook.com>
2023-01-28 20:29:09 -0300

    Feature hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+import Background from './Components/Background';
+import HeroText from './Components/HeroText';
 
 function App() {
-  return <h1>Hola</h1>;
+  return (
+    <div className="relative bg-main-bg">
+      <Background />
+      <HeroText />
+    </div>
+  );
 }
 
 export default App;

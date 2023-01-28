1db424212bddc4f22fdee784d742e0f861622ca8
West <agus2kcoin@outlook.com>
2023-01-28 20:27:15 -0300

    Feature hero text

---
+++ src/Components/HeroText.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+export default function TextSection() {
+  return (
+    <div className="hero-wrapper">
+      <h1 className="title">Three.js in React</h1>
+      <p className="description">
+        Bring your website to life with beautiful 3D objects
+      </p>
+    </div>
+  );
+}

6d9ec4219badb46de9f22509ef61e1520a876552
West <agus2kcoin@outlook.com>
2023-01-28 20:25:47 -0300

    Feature background

---
+++ src/Components/Background.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+export default function Background() {
+  return (
+    <div className="background-wrapper">
+      <div className="wave wave1"></div>
+      <div className="wave wave2"></div>
+      <div className="wave blur-layer"></div>
+      <div className="wave wave3"></div>
+      <div className="wave wave4"></div>
+      <div className="wave wave5"></div>
+      <div className="wave wave6"></div>
+    </div>
+  );
+}
